36e33f2f142874e1bba07592a918d4a3fe8aa209
Matilda Söderberg <matilda.89@live.se>
2023-10-24 19:31:24 +0200

    Refactor BirdController

--- Bird-API/Controllers/BirdsController.cs
+++ Bird-API/Controllers/BirdsController.cs
@@ -1,7 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using Bird_API.Data;
 using Bird_API.Models;
+using Bird_API.Interfaces;
 
 namespace Bird_API.Controllers
 {
@@ -9,55 +8,52 @@ namespace Bird_API.Controllers
     [ApiController]
     public class BirdsController : ControllerBase
     {
-        private readonly BirdContext _context;
+        private readonly IBirdRepo _repo;
 
-        public BirdsController(BirdContext context)
+        public BirdsController( IBirdRepo repo)
         {
-            _context = context;
+            _repo = repo;
         }
 
         [HttpGet]
         public async Task<IActionResult> ListAll()
         {
-            var result = await _context.Birds.ToListAsync();
+            var result = await _repo.ListAllAsync();
             return Ok(result);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetById(int id)
         {
-            var result = await _context.Birds.FindAsync(id);
+            var result = await _repo.FindByIdAsync(id);
             return Ok(result);
         }
         [HttpGet("breed/{name}")]
         public async Task<IActionResult> GetByName(string name)
         {
-            var result = await _context.Birds.FindAsync(name);
+            var result = await _repo.FindByNameAsync(name);
             return Ok(result);
         }
         [HttpPost()]
         public async Task<IActionResult> AddBird(Bird bird)
         {
-            var result = await _context.Birds.AddAsync(bird);
-            await _context.SaveChangesAsync();
+            var result = await _repo.AddAsync(bird);
             return Ok(result);
         }
         [HttpPut("{id}")]
         public async Task<IActionResult> UpdateBird(int id,Bird bird)
         {
-            var result = await _context.Birds.FindAsync(id);
+            var result = await _repo.FindByIdAsync(id);
             result.Name = bird.Name;
             result.Description = bird.Description;
             result.Habitat = bird.Habitat;
             result.ImageUrl = bird.ImageUrl;
-            _context.Birds.Update(result);
-            await _context.SaveChangesAsync();
+            await _repo.UpdateAsync(result);
             return NoContent();
         }
         [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteBird(int id, Bird bird)
+        public async Task<IActionResult> DeleteBird(int id)
         {
-            var result = await _context.Birds.FindAsync(id);
-            _context.Birds.Remove(result);
+            var result = await _repo.DeleteAsync(id);
             return NoContent();
         }
 
